894f482f3c325839778dc5032c970f090c8ea950
Nic Jansma <nic@nicj.net>
2020-01-30 12:41:36 -0500

    planet-images.js and planet-nearby.js added

---
+++ planet-images.js
@@ -0,0 +1,223 @@
+"use strict";
+
+//
+// Imports
+//
+const MediaWikiBot = require("nodemw");
+const async = require("async");
+const PlanetsLib = require("./planets-lib");
+const fs = require("fs");
+const path = require("path");
+
+//
+// Constants
+//
+const IMAGE_REGEX = /^\|\s*image\s*=.*\n/m;
+const CAPTION_REGEX = /^\|\s*caption\s*=.*\n/m;
+const PARALLEL = 1;
+const PRETEND = false;
+const SUC_YEAR = 3151;
+
+//
+// Command-Line
+//
+var onlySystem;
+if (process.argv.length >= 3) {
+    onlySystem = process.argv[2];
+    console.log(`Limiting to ${onlySystem}.`);
+}
+
+//
+// App
+//
+
+async.waterfall(
+    [
+        PlanetsLib.creds,
+        //
+        // Read planets file
+        //
+        function(creds, cb) {
+            PlanetsLib.read(onlySystem, function(err, systems, filteredSystems) {
+                cb(err, creds, systems, filteredSystems);
+            });
+        },
+        //
+        // Login
+        //
+        function(creds, systems, filteredSystems, cb) {
+            console.log("Logging in...");
+
+            // start MediaWikiBot with configuration
+            var client = new MediaWikiBot("config.json");
+
+            // login
+            client.logIn(creds.username, creds.password, function(err) {
+                cb(err, systems, filteredSystems, client);
+            });
+        },
+        //
+        // Ensure all images exist locally
+        //
+        function(systems, filteredSystems, client, cb) {
+            console.log("Ensuring images exist locally...");
+
+            async.eachLimit(filteredSystems, PARALLEL, function(system, cbEach) {
+                if (system.sarna.indexOf("%") !== -1) {
+                    // sarna link already is encoded, switch back to Unicode version for the real file
+                    system.imageName = `${decodeURIComponent(system.sarna)}_${SUC_YEAR}.svg`;
+
+                    // encoded version should be what we have on disk
+                    system.svgFileName = path.join(
+                        __dirname,
+                        "planet-images",
+                        SUC_YEAR.toString(),
+                        system.imageName);
+                } else {
+                    // sarna link is no encoded, use as-is
+                    system.imageName = `${system.sarna}_${SUC_YEAR}.svg`;
+
+                    // might need to encode for the disk version
+                    system.svgFileName = path.join(
+                        __dirname,
+                        "planet-images",
+                        SUC_YEAR.toString(),
+                        encodeURIComponent(system.imageName));
+                }
+
+                fs.access(system.svgFileName, fs.constants.R_OK, function(err) {
+                    if (err) {
+                        console.error(`Error with ${system.svgFileName}:`);
+                        console.error(err);
+                    }
+
+                    return cbEach(err);
+                });
+            }, function(err) {
+                cb(err, systems, filteredSystems, client);
+            });
+        },
+        //
+        // Read all files in
+        //
+        function(systems, filteredSystems, client, cb) {
+            console.log("Reading images in...");
+
+            async.eachLimit(filteredSystems, PARALLEL, function(system, cbEach) {
+                fs.readFile(system.svgFileName, function(err, data) {
+                    if (err) {
+                        console.error(`Error reading ${system.svgFileName}:`);
+                        console.error(err);
+                    }
+
+                    system.svgFileData = data;
+
+                    return cbEach(err);
+                });
+            }, function(err) {
+                cb(err, systems, filteredSystems, client);
+            });
+        },
+        //
+        // Ensure remote images exist, or upload
+        //
+        function(systems, filteredSystems, client, cb) {
+            console.log(`Uploading ${filteredSystems.length} systems's images...`);
+
+            async.eachLimit(filteredSystems, PARALLEL, function(system, cbEach) {
+                client.getImageInfo(`File:${system.imageName}`, function(err, imageinfo) {
+                    if (!imageinfo) {
+                        console.log(`\t${system.imageName} is missing, uploading`);
+                    } else {
+                        console.log(`\t${system.imageName} already exists`);
+
+                        // skip to next step
+                        return cbEach(err);
+                    }
+
+                    // image missing, upload
+                    return client.upload(
+                        system.imageName,
+                        system.svgFileData,
+                        `${system.name} neighboring systems (${SUC_YEAR})`,
+                        cbEach);
+                });
+            },
+            function(err) {
+                if (err) {
+                    console.error(err);
+                }
+
+                cb(err, systems, filteredSystems, client);
+            });
+        },
+        function(systems, filteredSystems, client, cb) {
+            console.log(`Updating ${filteredSystems.length} systems...`);
+
+            async.eachLimit(filteredSystems, PARALLEL, function(system, cbEach) {
+                console.log(`Checking ${system.name}: https://www.sarna.net/wiki/${system.sarna}`);
+
+                client.getArticle(system.sarna, function(err, data) {
+                    // error handling
+                    if (err) {
+                        return cbEach("Missing article!");
+                    }
+
+                    //
+                    // Image
+                    //
+                    IMAGE_REGEX.lastIndex = 0;
+
+                    const imageMatch = IMAGE_REGEX.exec(data);
+                    if (!imageMatch || !imageMatch.length) {
+                        return cbEach("Could not find an image");
+                    }
+
+                    let dataUpdated = data.replace(IMAGE_REGEX,
+                        `| image               = ${system.imageName}\n`);
+
+                    //
+                    // Caption
+                    //
+                    CAPTION_REGEX.lastIndex = 0;
+
+                    const captionMatch = CAPTION_REGEX.exec(data);
+                    if (!captionMatch || !captionMatch.length) {
+                        return cbEach("Could not find a caption");
+                    }
+
+                    dataUpdated = dataUpdated.replace(CAPTION_REGEX,
+                        `| caption             = ${system.name} [[#Nearby_Systems|nearby systems]] (${SUC_YEAR})\n`);
+
+                    PlanetsLib.diff(data, dataUpdated);
+
+                    if (!PRETEND) {
+                        return client.edit(
+                            system.sarna,
+                            dataUpdated,
+                            "Updating Planet coordinates per BattleTechWiki:Project_Planets/Mapping",
+                            true,
+                            cbEach);
+                    } else {
+                        return cbEach();
+                    }
+                });
+
+                return null;
+            }, function(err) {
+                if (err) {
+                    console.error(err);
+                }
+
+                cb();
+            });
+        }
+    ],
+    function(err) {
+        if (err) {
+            console.error(err);
+        }
+
+        console.log("Complete!");
+    }
+);
---
+++ planets-lib.js
@@ -0,0 +1,107 @@
+//
+// Imports
+//
+const async = require("async");
+const fs = require("fs");
+const prettydiff = require("prettydiff");
+
+//
+// Constants
+//
+const CREDS_FILE = "creds.json";
+const INPUT_FILE = "suc.tsv";
+
+// column numbers
+exports.COL_NAME = 1;
+exports.COL_ALT_NAME = 2;
+exports.COL_X = 3;
+exports.COL_Y = 4;
+exports.COL_LINK = 6;
+
+//
+// Exports
+//
+exports.creds = function(callback) {
+    if (!fs.existsSync(CREDS_FILE)) {
+        return callback(`${CREDS_FILE} doesn't exist!`);
+    }
+
+    let creds = JSON.parse(fs.readFileSync(CREDS_FILE));
+
+    return callback(null, creds);
+};
+
+exports.diff = function(before, after) {
+    let options = prettydiff.defaults;
+    options.source = before;
+    options.diff = after;
+    options.language = "text";
+    options.mode = "diff";
+
+    let diff = "(unknown)";
+    try {
+        diff = prettydiff.mode(options);
+    } catch (e) {
+        console.error(e);
+    }
+
+    console.log(diff);
+};
+
+exports.read = function(onlySystem, callback) {
+    async.waterfall([
+        function(cb) {
+            console.log(`Reading ${INPUT_FILE}...`);
+
+            // read input file
+            var inputSuc = fs.readFileSync(INPUT_FILE, "utf-8");
+
+            // split at each line
+            var inputLines = inputSuc.split("\n");
+
+            cb(null, inputLines);
+        },
+        function(inputLines, cb) {
+            console.log("Extracting coordinates...");
+
+            var systems = inputLines.map(line => {
+                const lineSplit = line.split("\t");
+
+                if (lineSplit.length < 6) {
+                    return null;
+                }
+
+                return {
+                    name: lineSplit[exports.COL_NAME].trim(),
+                    altName: lineSplit[exports.COL_ALT_NAME].trim(),
+                    x: parseFloat(lineSplit[exports.COL_X].replace("\"", "").replace(",", ""), 10),
+                    y: parseFloat(lineSplit[exports.COL_Y].replace("\"", "").replace(",", ""), 10),
+                    sarna: decodeURIComponent(lineSplit[exports.COL_LINK]
+                        .replace("https://www.sarna.net/wiki/", "")
+                        .replace("http://www.sarna.net/wiki/", ""))
+                };
+            }).filter(system => system !== null);
+
+            cb(null, systems);
+        },
+        function(systems, cb) {
+            // limit if desired
+            var filteredSystems = [];
+
+            if (!isNaN(parseInt(onlySystem, 0))) {
+                filteredSystems = systems.slice(0, parseInt(onlySystem, 0));
+            } else if (onlySystem) {
+                let foundSystem = systems.find(s => s.name === onlySystem || s.altName === onlySystem);
+                if (!foundSystem) {
+                    return callback(`Could not find system ${onlySystem}`);
+                }
+
+                filteredSystems.push(foundSystem);
+            } else {
+                filteredSystems = systems.slice(0);
+            }
+
+            return cb(null, systems, filteredSystems);
+        }
+    ], callback);
+};
